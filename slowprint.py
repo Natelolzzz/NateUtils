692b88755bcd7eb217aac6c6eab54c84292effdd
Natelolzzz <98819207+Natelolzzz@users.noreply.github.com>
2024-10-11 15:18:04 +0100

    Create slowprint.py

---
+++ slowprint.py
@@ -0,0 +1,12 @@
+import random
+import sys
+import time
+
+
+def print_slow(string, typing_speed):
+  string = str(string)
+  string += "\n"
+  for letter in string:
+    sys.stdout.write(letter)
+    sys.stdout.flush()
+    time.sleep(random.random() * 10.0 / typing_speed)
