36c6048139b5a8497b6f684445d9cd3d75ca0204
Natelolzzz <98819207+Natelolzzz@users.noreply.github.com>
2024-10-16 11:32:40 +0100

    Create setup.py

---
+++ .github/workflows/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='NateUtils',
+      version='1.0',
+      description='Various Python Utilities',
+      author='Natelolzzz',
+      packages=['distutils', 'sys', 'random', 'time']
+)
