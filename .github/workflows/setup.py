6ef0bb5c7f3141a3598d6266a4ba36f4f827e9a9
Natelolzzz <98819207+Natelolzzz@users.noreply.github.com>
2024-10-16 11:35:59 +0100

    Delete .github/workflows directory

--- .github/workflows/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-
-setup(name='NateUtils',
-      version='1.0',
-      description='Various Python Utilities',
-      author='Natelolzzz',
-      packages=['distutils', 'sys', 'random', 'time']
-)
