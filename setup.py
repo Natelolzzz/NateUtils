d3c4c08eb4f01c50b4bb04807f4f51ed902aab74
Natelolzzz <98819207+Natelolzzz@users.noreply.github.com>
2024-10-16 11:59:22 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name='NateUtils',
     version='1.0',
-    py_modules=['sys','random','time'],  # Replace with your actual module names
+    py_modules=['sys','random','time'], 
 )
