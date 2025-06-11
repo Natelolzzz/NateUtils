dd044bbcf8dbfb1ac5140e9e716f028dec055431
Natelolzzz <98819207+Natelolzzz@users.noreply.github.com>
2025-06-11 12:49:21 +0100

    Update NateUtils.py

--- NateUtils.py
+++ NateUtils.py
@@ -4,7 +4,8 @@ import time
 from sys import stdout
 from datetime import datetime
 
-def printSlow(string, typing_speed=300):
+# Generic Utils
+def printSlow(string, typing_speed=300): # A horrific crime, which I am only putting here since I seem to use it in every project. Stolen off stackoverflow and bent to my whims shamelessly
   string = str(string)
   string += "\n"
   for letter in string:
@@ -12,26 +13,30 @@ def printSlow(string, typing_speed=300):
     sys.stdout.flush()
     time.sleep(random.random() * 10.0 / typing_speed)
 
-def isEven(num):
+def currentTime(): # Kinda handy, use it sometimes
+  now = datetime.now()
+  return now.strftime("%H:%M:%S")
+
+# Maths Utils
+def isEven(num): # You'd be surprised how much I use this
   if num % 2:
       return False
   else:
       return True
 
-def isOdd(num):
-  return not iseven(num)
+def isOdd(num): # :)
+  return not(iseven(num))
 
-def clamp(num, lowest=0):
-  if num < lowest:
-    num = lowest
-  return num
+def average(list): # Commonly used, very handy
+    return sum(list) / len(list) 
 
-def readLine(path, line):
+# File Utils
+def readLine(path, line): # self explanatory
   file = open(path) 
   content = file.readlines() 
   return content[line-1]
 
-def readLines(path, lines):
+def readLines(path, lines): # self explanatory
   content = ""
   if len(lines) > 0:
     line = lines.pop()
@@ -39,15 +44,14 @@ def readLines(path, lines):
     content += "\n"
   return content
 
-def currentTime():
-  now = datetime.now()
-  return now.strftime("%H:%M:%S")
-
-def rangeCheck(lower, upper, value):
+# Validation Utils
+def rangeCheck(lower, upper, value): # Check if an integer is inbetween two other integers
   try:
     return float(lower) <= float(value) <= float(upper)
   except:
     return False
-    
-def average(list): 
-    return sum(list) / len(list) 
+
+def clamp(num, lowest=0): # Basically just another input validation thing
+  if num < lowest:
+    num = lowest
+  return num
